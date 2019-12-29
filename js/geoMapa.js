900990e9ce1e23bd4049e3fa64e9724ca20bd3aa
Joan Lluís <juanlufd@gmail.com>
2019-12-29 01:51:43 +0100

    selector de mapa base

---
+++ js/geoMapa.js
@@ -0,0 +1,41 @@
+var map = L.map('map').setView([40, -3], 7);
+
+var layer = L.esri.basemapLayer('Imagery').addTo(map);
+var layerLabels = L.esri.basemapLayer('ImageryLabels').addTo(map);
+
+L.control.scale().addTo(map);
+
+function setBasemap(basemap) {
+    if (layer) {
+        map.removeLayer(layer);
+    }
+
+    layer = L.esri.basemapLayer(basemap);
+
+    map.addLayer(layer);
+
+    if (layerLabels) {
+        map.removeLayer(layerLabels);
+    }
+
+    if (
+        basemap === 'ShadedRelief' ||
+        basemap === 'Oceans' ||
+        basemap === 'Gray' ||
+        basemap === 'DarkGray' ||
+        basemap === 'Terrain'
+    ) {
+        layerLabels = L.esri.basemapLayer(basemap + 'Labels');
+        map.addLayer(layerLabels);
+    } else if (basemap.includes('Imagery')) {
+        layerLabels = L.esri.basemapLayer('ImageryLabels');
+        map.addLayer(layerLabels);
+    }
+}
+
+document
+    .querySelector('#basemaps')
+    .addEventListener('change', function (e) {
+        var basemap = e.target.value;
+        setBasemap(basemap);
+    });
