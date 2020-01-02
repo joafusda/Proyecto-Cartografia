0bb48b605c99dc04d39b4b8ecc75e5f6d401e46f
Joan Lluís <juanlufd@gmail.com>
2020-01-02 14:06:51 +0100

    archivos de pruebas eliminados

--- js/geoMapa.js
+++ js/geoMapa.js
@@ -51,12 +51,12 @@ checkbox.addEventListener('change', (event) => {
     if (event.target.checked) {
 
         /* GeoTIFF */
-        var url_to_geotiff_file = "https://joafusda.upv.edu.es/data/antes_B8A.tif";
+        var url_to_geotiff_file = "data/antes_B8A.tif";
 
         fetch(url_to_geotiff_file).then(r => r.arrayBuffer()).then(function (buffer) {
             var s = L.ScalarField.fromGeoTIFF(buffer);
             let layer = L.canvasLayer.scalarField(s).addTo(map);
-            
+
             layer.on("click", function (e) {
                 if (e.value !== null) {
                     let popup = L.popup()
@@ -68,38 +68,6 @@ checkbox.addEventListener('change', (event) => {
 
             map.fitBounds(layer.getBounds());
         });
-
-
-        // fetch(url_to_geotiff_file)
-        //     .then(response => response.arrayBuffer())
-        //     .then(arrayBuffer => {
-        //         parseGeoraster(arrayBuffer).then(georaster => {
-        //             var layer = L.leafletGeotiff(
-        //                 url = url_to_geotiff_file,
-        //                 options = {
-        //                     displayMin: 0,
-        //                     displayMax: 30,
-        //                     name: 'capa1',
-        //                     colorScale: 'rainbow',
-        //                     clampLow: false,
-        //                     clampHigh: true
-        //                 });
-        //             layer.addTo(map);
-
-        //             layer.on('click', function (e) {
-        //                 if (e !== null) {
-        //                     let v = georaster.getValueAtLatLng(e.latlng.lat, e.latlng.lng);;
-        //                     let html = (`<span class="popupText">Valor ${v} m</span>`);
-        //                     let popup = L.popup()
-        //                         .setLatLng(e.latlng)
-        //                         .setContent(html)
-        //                         .openOn(map);
-        //                 }
-        //             });
-
-        //             map.fitBounds(layer.getBounds());
-        //         });
-        //     });
 
     } else {
         alert('not checked');
